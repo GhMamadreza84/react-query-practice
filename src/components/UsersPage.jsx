ecf244747e0f8a573d635fb085651ffc1a0e45f4
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-27 13:34:25 -0700

    edit url in userpage

--- src/components/UsersPage.jsx
+++ src/components/UsersPage.jsx
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 const UsersPage = () => {
   const fetchUsers = async () => {
-    const res = await axios.get("https://jasonplaceholder.typicode.com/users");
+    const res = await axios.get("https://jsonplaceholder.typicode.com/users");
     return res.data;
   };
 
