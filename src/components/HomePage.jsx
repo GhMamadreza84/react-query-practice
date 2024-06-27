2c0dd863ff813ad33f2df540e81cc92a4c3245c6
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-27 12:23:37 -0700

    restore to defualt home page component

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,23 +1,9 @@
-import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
+
 const HomePage = () => {
-  const fetchUsers = async () => {
-    const res = await axios.get("https://jsonplaceholder.typicode.com/users");
-    return res.data;
-  };
-
-  const { data, isLoading, isError, error } = useQuery(["users"], fetchUsers);
-
-  // console.log(isError , error);
-  if (isLoading) return <h1>Loading ...</h1>;
-  if(isError) return <h1>Something Went Wrong  - {error.message}</h1>
   return (
     <div>
-      {data.map((i) => (
-        <h4 key={i.id}>{i.name}</h4>
-      ))}
     </div>
   );
-};
+}
 
 export default HomePage;
