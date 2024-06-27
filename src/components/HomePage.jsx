d750db322c4ac1983d97e76c3f232fe5c72d207d
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-27 12:14:37 -0700

    edit url for get

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 const HomePage = () => {
   const fetchUsers = async () => {
-    const res = await axios.get("https://ajsonplaceholder.typicode.com/users");
+    const res = await axios.get("https://jsonplaceholder.typicode.com/users");
     return res.data;
   };
 
