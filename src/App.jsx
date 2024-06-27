c703e78feca19dd8d288006cafcad2fe72be00fb
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-27 12:28:18 -0700

    edit apps

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Routes ,Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import HomePage from "./components/HomePage";
