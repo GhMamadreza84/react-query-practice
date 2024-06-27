cb9c3ba18fd1d8e9c81bcc9a6fd61ca87a97509d
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-27 12:25:18 -0700

    import and use react-query-devtools in app

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import HomePage from "./components/HomePage";
 import { Route, Routes } from "react-router-dom";
 import UsersPage from "./components/UsersPage";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function App() {
   const queryCLient = new QueryClient();
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/users" element={<UsersPage />} />
         </Routes>
+        <ReactQueryDevtools />
       </QueryClientProvider>
     </>
   );
