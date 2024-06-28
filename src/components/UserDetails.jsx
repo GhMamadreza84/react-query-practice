88e447f5dfe39b0d1d2e87381cdcb08491b6f635
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-28 04:24:49 -0700

    get data with usequery hook in userdetails

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,7 +1,9 @@
+import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 
 const UserDetails = () => {
   const { id } = useParams();
+  const { data } = useQuery(["users", id], fetchUserDetails);
   return <div>UserDetails - ID : {id}</div>;
 };
 
