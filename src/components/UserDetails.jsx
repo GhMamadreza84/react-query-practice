7507bab38541a243b6bc5f892f0a5d0693b80a96
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-28 04:32:49 -0700

    edit userdetails component

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -5,14 +5,23 @@ import { useParams } from "react-router-dom";
 const UserDetails = () => {
   const { id } = useParams();
   const fetchUserDetails = async () => {
-    return await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
+    const res = await axios.get(
+      `https://jsonplaceholder.typicode.com/users/${id}`
+    );
+    return res.data;
   };
   const { data } = useQuery(["users", id], fetchUserDetails);
   console.log(data);
   return (
     <div>
       UserDetails - ID : {id}
-      
+      {data.map((i) => {
+        <div key={i.id}>
+          <h2>{i.name}</h2>
+          <h3>{i.username}</h3>
+          <h4>{i.email}</h4>
+        </div>;
+      })}
     </div>
   );
 };
