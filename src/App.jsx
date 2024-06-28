aa947c2f2d90a79084005a93a590b24ec04e076b
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-28 05:18:02 -0700

    create form component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { Routes, Route, Router } from "react-router-dom";
+import { Routes, Route} from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import HomePage from "./components/HomePage";
 import UsersPage from "./components/UsersPage";
 import UserDetails from "./components/UserDetails";
+import Form from "./components/Form";
 
 function App() {
   const queryCLient = new QueryClient();
@@ -14,6 +15,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/users" element={<UsersPage />} />
           <Route path="/users/:id" element={<UserDetails />} />
+          <Route path="/users/:id" element={<Form />} />
         </Routes>
         <ReactQueryDevtools />
       </QueryClientProvider>
---
+++ src/components/Form.jsx
@@ -0,0 +1,10 @@
+
+const Form = () => {
+  return (
+    <div>
+      Form
+    </div>
+  );
+}
+
+export default Form;
