677ba0e89b75b9bd32ad6535db83c2216dbac379
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-28 04:26:27 -0700

    i create fetchuserdetails function for get data with axios from json place holder

--- src/components/UserDetails.jsx
+++ src/components/UserDetails.jsx
@@ -1,8 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
 import { useParams } from "react-router-dom";
 
 const UserDetails = () => {
   const { id } = useParams();
+  const fetchUserDetails = async () => {
+    return await axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
+  };
   const { data } = useQuery(["users", id], fetchUserDetails);
   return <div>UserDetails - ID : {id}</div>;
 };
