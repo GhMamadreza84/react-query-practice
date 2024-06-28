1d322fcb7795868c0f5a793ef8bc168ed709db12
ghmamadreza2 <ghmamadreza84@gmail.com>
2024-06-28 04:20:41 -0700

    i added new route in app for userdetails

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import HomePage from "./components/HomePage";
 import UsersPage from "./components/UsersPage";
+import UserDetails from "./components/UserDetails";
 
 function App() {
   const queryCLient = new QueryClient();
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/users" element={<UsersPage />} />
+          <Route path="/users/:id" element={<UserDetails />} />
         </Routes>
         <ReactQueryDevtools />
       </QueryClientProvider>
